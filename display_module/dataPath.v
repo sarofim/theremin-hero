b136143176cd9a3a8560fd800c6d96e0ba488c5b
Seif <seif.a.sarofim@gmail.com>
2017-11-19 13:52:13 -0500

    updated dataPath.v
    removed memory blocks: startAddress implemented with mux, loadblack for default image

--- display_module/dataPath.v
+++ display_module/dataPath.v
@@ -1,12 +1,12 @@
-module dataPath(input clock, reset, shiftSong, writeToScreen, loadStartAddress, loadX, loadY, loadDefault, writeDefault,
-                input [15:0] gridCounter, input [3:0] boxCounter, input [15:0] pixelCount,
-                output reg [7:0] vgaOutX, output reg [6:0] vgaOutY, output reg [2:0] vgaOutColour);
-					 
+module dataPath(input clock, reset, shiftSong, writeToScreen, loadStartAddress, loadX, loadY, loadDefault, writeDefault, songDone,
+                input [15:0] gridCounter, input [1:0] boxCounter, input [15:0] pixelCount,
+                output reg [8:0] vgaOutX, output reg [7:0] vgaOutY, output reg [2:0] vgaOutColour);
+
   //Resolution  = 320 * 240; 76800 = 17b'10010110000000000 (17bits)
   //writing to 240*180 grid; 43200 = 15b'101,01000,1100,0000 (15bits)
   reg [14:0] regAddress;
-  reg [7:0] regX;
-  reg [6:0] regY;
+  reg [8:0] regX;
+  reg [7:0] regY;
   reg [2:0] regColour;
   wire [14:0] wireAddressOut;
   reg [14:0] currentAddress;
@@ -16,37 +16,43 @@ module dataPath(input clock, reset, shiftSong, writeToScreen, loadStartAddress, 
   reg [3:0] shiftedRegNote1, shiftedRegNote2, shiftedRegNote3;
   reg currentNote1, currentNote2, currentNote3;
   always@(posedge clock) begin
-    if(reset) begin
+    if(reset || songDone) begin
       regNote1 <= 4'b0010;
       regNote2 <= 4'b0100;
       regNote3 <= 4'b1000;
       end
     else if(shiftSong) begin
       //set rightmost node as current note
-      currentNote1 = regNote1[0];
-      currentNote2 = regNote2[0];
-      currentNote3 = regNote3[0];
+      currentNote1 <= regNote1[0];
+      currentNote2 <= regNote2[0];
+      currentNote3 <= regNote3[0];
 
       //shift all registers right
-      shiftedRegNote1 = regNote1 >> 1;
-      shiftedRegNote2 = regNote2 >> 1;
-      shiftedRegNote3 = regNote3 >> 1;
-
-      //set leftmost to current value
-      regNote1[3] = currentNote1;
-      regNote2[3] = currentNote2;
-      regNote3[3] = currentNote3; 
-      end 
+      shiftedRegNote1 <= regNote1 >> 1'b1;
+      shiftedRegNote2 <= regNote2 >> 1'b1;
+      shiftedRegNote3 <= regNote3 >> 1'b1;
+      end
   end
 
   reg colourSelect;
-  //noteSelect mux
+  //noteSelect mux - loads start address for each box
   always@(posedge clock) begin
     case(boxCounter)
-      0: colourSelect <= currentNote1;
-      1: colourSelect <= currentNote2;
-      2: colourSelect <= currentNote3;
-      default: colourSelect <= 0;
+      0: begin
+         colourSelect <= currentNote1;
+         currentAddress <= 15'b101101000000000;
+         end
+      1: begin
+         colourSelect <= currentNote2;
+         currentAddress <= 15'b101101000111100;
+      2: begin
+         colourSelect <= currentNote3;
+         currentAddress <= 15'b101101001111000;
+         end
+      default: begin
+         colourSelect <= 0;
+         currentAddress <= 15'd0;
+         end
     endcase
   end
 
@@ -56,12 +62,6 @@ module dataPath(input clock, reset, shiftSong, writeToScreen, loadStartAddress, 
     if(colourSelect) regInColour <= 3'b111; /*load white*/
     else regInColour <= 3'b000; /*load black*/
   end
-
-  wire [4:0] tempMemAddress;
-  assign tempMemAddress = {1'b0, boxCounter};
-  //address memory register : 15 bit wide, 12 rows (12*15)
-  startAddressMem memA(.clock(clock), .address(tempMemAddress), .data(15'd0),
-                       .wren(1'b0), .q(wireAddressOut));
 
   always@(posedge clock) begin
     if(reset) begin
@@ -80,25 +80,28 @@ module dataPath(input clock, reset, shiftSong, writeToScreen, loadStartAddress, 
   //regX & regY
   always @(posedge clock) begin
     if (!reset) begin
-      regX <= 8'd0;
-      regY <= 7'd0;
+      regX <= 9'd0;
+      regY <= 8'd0;
       end
-    else if (loadX) regX <= currentAddress[14:7];
-    else if (loadY) regY <= currentAddress[6:0];
+    else if (loadX && loadY) begin
+      regX <= {1'b0, currentAddress[14:7]};
+      regX <= {1'b0, currentAddress[6:0]};
+      end
   end
 
   //loading default image
   //memory address 15bits for position - added 3 for color
   //required memory block 43200*15bits (43200 = 16'b1010100011000000)
   // closest mem size 65536 = 17'b10000000000000000
-  wire [17:0] defaultImageAddress;
-  reg [7:0] regDefaultX;
-  reg [6:0] regDefaultY;
+  reg [8:0] regDefaultX;
+  reg [7:0] regDefaultY;
   reg [2:0] regDefaultColour;
-  wire [17:0] tempMemAddress1;
-  assign tempMemAddress1 = {1'b0, gridCounter}; 
-  startImageAddress memD(.address(tempMemAddress1), .clock(clock), .data(18'd0),
-                         .wren(1'b0), .q(defaultImageAddress));
+  //memory block to be used in later implementations
+  // wire [17:0] defaultImageAddress;
+  // wire [16:0] tempMemAddress1;
+  // assign tempMemAddress1 = {1'b0, gridCounter};
+  // startImageAddress memD(.address(tempMemAddress1), .clock(clock), .data(18'd0),
+  //                        .wren(1'b0), .q(defaultImageAddress));
 
   //default registers stuff
   always @(posedge clock) begin
@@ -108,9 +111,9 @@ module dataPath(input clock, reset, shiftSong, writeToScreen, loadStartAddress, 
       regDefaultColour <= 3'd0;
       end
     else if (loadDefault) begin
-      regDefaultX <= defaultImageAddress[17:10];
-      regDefaultY <= defaultImageAddress[9:3];
-      regDefaultColour <= defaultImageAddress[2:0];
+      regDefaultX <= {1'b0, gridCounter[14:7] /*defaultImageAddress[17:10]*/};
+      regDefaultY <= {1'b0, gridCounter[6:0] /*defaultImageAddress[9:3]*/};
+      regDefaultColour <= 3'b000 /*defaultImageAddress[2:0]*/;
       end
   end
 
@@ -118,13 +121,13 @@ module dataPath(input clock, reset, shiftSong, writeToScreen, loadStartAddress, 
   //vgaOut = starting position of square (0, 120) + regX/Y
   always@(posedge clock) begin
     if(writeToScreen && writeDefault) begin
-      vgaOutX <= 0 + regDefaultX;
-      vgaOutY <= 120 + regDefaultY;
+      vgaOutX <= 9'd0 + regDefaultX;
+      vgaOutY <= 8'd120 + regDefaultY;
       vgaOutColour <= regDefaultColour;
       end
     else if(writeToScreen) begin
-      vgaOutX <= 0 + regX;
-      vgaOutY <= 120 + regY;
+      vgaOutX <= 9'd0 + regX;
+      vgaOutY <= 8'd120 + regY;
       vgaOutColour <= regDefaultColour;
       end
   end
